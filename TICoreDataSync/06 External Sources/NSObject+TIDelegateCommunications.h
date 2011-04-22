b2a478a8988d591759f20fdaec096eb80aa8b283
Tim Isted <git@timisted.com>
2011-04-22 11:50:36 +0100

    update delegate communications category

--- TICoreDataSync/06 External Sources/NSObject+TIDelegateCommunications.h
+++ TICoreDataSync/06 External Sources/NSObject+TIDelegateCommunications.h
@@ -1,15 +1,28 @@
-//
-//  NSObject+TIDelegateCommunications.h
-//  ShoppingListMac
-//
-//  Created by Tim Isted on 21/04/2011.
-//  Copyright 2011 Tim Isted. All rights reserved.
-//
+// Copyright (c) 2011 Tim Isted
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 
 @interface NSObject (TIDelegateCommunications)
 
 /** Returns an invocation confgured to target the delegate */
-- (NSInvocation *)ti_invocationForDelegateSelector:(SEL)aSelector;
+- (NSInvocation *)ti_invocationForDelegateSelector:(SEL)aSelector withArgList:(va_list)args;
 
 /** Method used primarily by others to get some kind of result from a delegate selector passing a variable number of (object) arguments. Returns YES if invocation was invoked successfully; result parameter won't be changed if invocation not invoked */
 - (BOOL)ti_getResult:(void *)result fromDelegateWithSelector:(SEL)aSelector, ...;
@@ -19,9 +32,6 @@
 
 /** Returns an object value from a given delegate selector and provided (object) arguments */
 - (id)ti_objectFromDelegateWithSelector:(SEL)aSelector, ...;
-
-/** Returns a BOOL value from a given delegate selector; indicate optimistic to return YES if the delegate doesn't respond to the selector */
-- (BOOL)ti_optimistic:(BOOL)optimistic boolFromDelegateSelector:(SEL)aSelector, ...;
 
 /** Returns a BOOL value from a given delegate selector. If the delegate does not respond to the selector, YES is returned. */
 - (BOOL)ti_optimisticBoolFromDelegateSelector:(SEL)aSelector, ...;
--- TICoreDataSync/06 External Sources/NSObject+TIDelegateCommunications.m
+++ TICoreDataSync/06 External Sources/NSObject+TIDelegateCommunications.m
@@ -1,18 +1,30 @@
-//
-//  NSObject+TIDelegateCommunications.m
-//  ShoppingListMac
-//
-//  Created by Tim Isted on 21/04/2011.
-//  Copyright 2011 Tim Isted. All rights reserved.
-//
+// Copyright (c) 2011 Tim Isted
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 
 #import "NSObject+TIDelegateCommunications.h"
 
 
 @implementation NSObject (TIDelegateCommunications)
 
-#pragma mark Invocations
-- (NSInvocation *)ti_invocationForDelegateSelector:(SEL)aSelector
+#pragma mark Invocation Generation
+- (NSInvocation *)ti_invocationForDelegateSelector:(SEL)aSelector withArgList:(va_list)args
 {
     if( !aSelector ) {
         return nil;
@@ -26,24 +38,12 @@
         return nil;
     }
     
-    NSMethodSignature *signature = [[[(id)self delegate] class] instanceMethodSignatureForSelector:aSelector];
-    NSInvocation *selectorInvocation = [NSInvocation invocationWithMethodSignature:signature];
-    [selectorInvocation setSelector:aSelector];
-    [selectorInvocation setTarget:[(id)self delegate]];
-    [selectorInvocation setArgument:&self atIndex:2];
-    
-    return selectorInvocation;
-}
-
-- (NSInvocation *)ti_invocationForDelegateSelector:(SEL)aSelector withArgList:(va_list)args
-{
-    NSInvocation *invocation = [self ti_invocationForDelegateSelector:aSelector];
-    if( !invocation ) {
-        return nil;
-    }
-    
-    NSMethodSignature *methodSignature = [invocation methodSignature];
-    
+    NSMethodSignature *methodSignature = [[[(id)self delegate] class] instanceMethodSignatureForSelector:aSelector];
+    NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:methodSignature];
+    [invocation setSelector:aSelector];
+    [invocation setTarget:[(id)self delegate]];
+    [invocation setArgument:&self atIndex:2];
+        
     id eachArgument = nil;
     for( int argumentIndex = 3; argumentIndex < [methodSignature numberOfArguments]; argumentIndex++ ) {
         eachArgument = va_arg(args, id);
@@ -100,19 +100,6 @@
 
 #pragma mark -
 #pragma mark Requesting Boolean Values
-- (BOOL)ti_optimistic:(BOOL)optimistic boolFromDelegateSelector:(SEL)aSelector, ...
-{
-    BOOL result = optimistic;
-    
-    va_list args;
-    va_start(args, aSelector);
-    
-    [self ti_getResult:&result fromDelegateWithSelector:aSelector withArgList:args];
-    va_end(args);
-
-    return result;
-}
-
 - (BOOL)ti_optimisticBoolFromDelegateSelector:(SEL)aSelector, ...
 {
     BOOL result = YES;
