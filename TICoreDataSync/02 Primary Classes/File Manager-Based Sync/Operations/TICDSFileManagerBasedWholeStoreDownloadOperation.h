ba82cf99510924179eb0f7e0486b017c80e165e5
Tim Isted <git@timisted.com>
2011-04-26 14:19:41 +0100

    documentation for file manager based whole store download operation

--- TICoreDataSync/02 Primary Classes/File Manager-Based Sync/Operations/TICDSFileManagerBasedWholeStoreDownloadOperation.h
+++ TICoreDataSync/02 Primary Classes/File Manager-Based Sync/Operations/TICDSFileManagerBasedWholeStoreDownloadOperation.h
@@ -8,15 +8,23 @@
 
 #import "TICDSWholeStoreDownloadOperation.h"
 
-
+/**
+ `TICDSFileManagerBasedWholeStoreDownloadOperation` is a "whole store download" operation designed for use with a `TICDSFileManagerBasedDocumentSyncManager`.
+ */
 @interface TICDSFileManagerBasedWholeStoreDownloadOperation : TICDSWholeStoreDownloadOperation {
 @private
     NSString *_thisDocumentWholeStoreDirectoryPath;
 }
 
+/** @name Paths */
+
+/** The path to a given client's `WholeStore.sqlite` file within this document's `WholeStore` directory. */
 - (NSString *)pathToWholeStoreFileForClientWithIdentifier:(NSString *)anIdentifier;
+
+/** The path to a given client's `AppliedSyncChanges.sqlite` file within this document's `WholeStore` directory .*/
 - (NSString *)pathToAppliedSyncChangesFileForClientWithIdentifier:(NSString *)anIdentifier;
 
+/** The path to this document's `WholeStore` directory. */
 @property (retain) NSString *thisDocumentWholeStoreDirectoryPath;
 
 @end
